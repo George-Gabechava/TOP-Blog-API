94af381a96aa54c35c00e0dd12a4fc8ad04dc48e
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2025-12-17 09:47:46 -0500

    updated settings for Railway

--- bloggerFrontend/vite.config.js
+++ bloggerFrontend/vite.config.js
@@ -5,10 +5,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     cors: {
       // the origin you will be accessing via browser
       origin: "http://localhost:5000/",
     },
+  },
+  preview: {
+    host: true,
+    port: process.env.PORT ? Number(process.env.PORT) : 4173,
+    strictPort: true,
+    allowedHosts: ["top-blog-api-bloggerfrontend-production.up.railway.app"],
   },
   build: {
     // generate .vite/manifest.json in outDir
