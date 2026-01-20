c30c9a7ef0557790247d3ceea95abdebcc3567fa
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2026-01-20 15:51:10 -0500

    cors changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
-app.options(/.*/, cors(corsOptions));
+app.options(/"*"/, cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
